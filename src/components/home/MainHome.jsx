e9ad9ce504174b7227e99b7a2211d7048813db23
Federico <federeinoso01@gmail.com>
2022-10-18 03:43:18 -0300

    grid fix

--- src/components/home/MainHome.jsx
+++ src/components/home/MainHome.jsx
@@ -3,13 +3,13 @@ import ImgAbout2 from "../../assets/images/image-about-light.jpg";
 
 const MainHome = () => {
   return (
-    <main className="grid grid-cols-1 lg:grid-cols-7 h-full">
+    <main className="grid grid-cols-1 lg:grid-cols-7">
       <img
         src={ImgAbout1}
         alt=""
-        className="object-cover lg:col-span-2 w-full"
+        className="object-cover lg:col-span-2 h-full w-full"
       />
-      <section className="px-7 py-10 lg:col-span-3 grid place-content-center lg:px-20">
+      <section className="px-7 py-10 lg:col-span-3 flex flex-col place-content-center lg:px-20">
         <h2 className="uppercase font-bold tracking-[0.5rem] text-sm mb-3">
           About our furniture
         </h2>
@@ -25,7 +25,7 @@ const MainHome = () => {
       <img
         src={ImgAbout2}
         alt=""
-        className="object-cover lg:col-span-2 w-full"
+        className="object-cover lg:col-span-2 h-full w-full"
       />
     </main>
   );
