e9bd216c901a277e87a5531a8a8353ebbde09379
Federico <federeinoso01@gmail.com>
2022-10-19 02:42:45 -0300

    mobile fix

--- src/components/home/MainArticle.jsx
+++ src/components/home/MainArticle.jsx
@@ -6,7 +6,7 @@ const MainArtileHome = ({ hero }) => {
       <img
         src={window.innerWidth < 500 ? `${hero.imgMobile}` : `${hero.img}`}
         alt={hero.title}
-        className="object-cover col-start-1 col-end-4 row-start-1 row-end-3 lg:col-start-1 lg:col-end-5  lg:row-start-1 lg:row-end-4 w-full h-[40vh] lg:h-[70vh]"
+        className="object-cover col-start-1 col-end-4 row-start-1 row-end-3 lg:col-start-1 lg:col-end-5  lg:row-start-1 lg:row-end-4 w-full h-fit lg:h-[70vh]"
       />
       <article className="py-10 px-7 col-start-1 col-end-4 lg:col-start-5 lg:col-end-8 lg:row-start-1 lg:row-end-2 lg:px-20 flex flex-col justify-center lg:h-[60vh]">
         <h1 className="font-medium text-3xl lg:text-5xl mb-4">{hero.title}</h1>
